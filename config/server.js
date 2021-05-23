3e63fbed4a23bf972fe265abb304cf3d90d36c92
ruk91 <sajithkasn@gmail.com>
2021-05-23 23:41:51 +0530

    Begin project 🎉

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e5d1d4d159d04bf057ef88833c447f02'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7db0f987-a4c1-4bb8-a1b8-79f6279d6bc5'
+};
